8b775947bca5be54beb98caf0982af28ea4a449c
Muhammad Usman <usmn.ali178@gmail.com>
2022-08-30 13:20:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+
+document.querySelector(".w").addEventListener("click", handleclick );
+
+function handleclick (){
+  var sound = new Audio ("sounds/crash.mp3");
+  sound.play();
+
+}
+
+document.querySelector(".a").addEventListener("click", handleclick1 );
+
+function handleclick1 (){
+  var sound = new Audio ("sounds/kick-bass.mp3");
+  sound.play();
+
+}
+
+
+document.querySelector(".s").addEventListener("click", handleclick2 );
+
+function handleclick2 (){
+  var sound = new Audio ("sounds/snare.mp3");
+  sound.play();
+
+}
+
+
+document.querySelector(".d").addEventListener("click", handleclick3 );
+
+function handleclick3 (){
+  var sound = new Audio ("sounds/tom-1.mp3");
+  sound.play();
+
+}
+
+
+document.querySelector(".j").addEventListener("click", handleclick4 );
+
+function handleclick4 (){
+  var sound = new Audio ("sounds/tom-2.mp3");
+  sound.play();
+
+}
+
+
+document.querySelector(".k").addEventListener("click", handleclick5 );
+
+function handleclick5 (){
+  var sound = new Audio ("sounds/tom-3.mp3");
+  sound.play();
+
+}
+
+
+document.querySelector(".l").addEventListener("click", handleclick6 );
+
+function handleclick6 (){
+  var sound = new Audio ("sounds/tom-4.mp3");
+  sound.play();
+
+}
+
+
+document.querySelector(".w").addEventListener("click", handleclick );
+
+function handleclick (){
+  var sound = new Audio ("sounds/crash.mp3");
+  sound.play();
+
+}
